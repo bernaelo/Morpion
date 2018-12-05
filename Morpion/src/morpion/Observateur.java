24b3301fcdc2be9658cb25cf39a83b13f1296c15
bernaelo <45598228+bernaelo@users.noreply.github.com>
2018-12-05 11:27:20 +0100

    Add files via upload

---
+++ Morpion/src/morpion/Observateur.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package morpion;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author bernaelo
+ */
+public class Observateur implements Observer {
+
+    @Override
+    public void update(Observable arg0, Object arg1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
